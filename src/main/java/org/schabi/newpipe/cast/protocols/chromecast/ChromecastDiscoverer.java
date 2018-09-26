937cbff94da9352d1ce57f5ffefd08c0b073f901
Joe Tortorello <32490063+NotTheRealJoe@users.noreply.github.com>
2018-09-26 11:23:39 -0500

    Add class for discovering chromecasts

---
+++ src/main/java/org/schabi/newpipe/cast/protocols/chromecast/ChromecastDiscoverer.java
@@ -0,0 +1,14 @@
+package org.schabi.newpipe.cast.protocols.chromecast;
+
+import org.schabi.newpipe.cast.Device;
+import org.schabi.newpipe.cast.Discoverer;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class ChromecastDiscoverer extends Discoverer {
+    public List<Device> discoverDevices() throws IOException, InterruptedException, ExecutionException {
+
+    }
+}
