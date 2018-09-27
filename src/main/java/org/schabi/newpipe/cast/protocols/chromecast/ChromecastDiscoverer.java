bc9c3dc61ffedcfc5bb61eb49cbc30d0357379b1
NotTheRealJoe <32490063+NotTheRealJoe@users.noreply.github.com>
2018-09-27 11:09:03 -0500

    Get library actually imported correctly

--- src/main/java/org/schabi/newpipe/cast/protocols/chromecast/ChromecastDiscoverer.java
+++ src/main/java/org/schabi/newpipe/cast/protocols/chromecast/ChromecastDiscoverer.java
@@ -7,8 +7,14 @@ import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+import su.litvak.chromecast.api.v2.ChromeCasts;
+import su.litvak.chromecast.api.v2.ChromeCast;
+
 public class ChromecastDiscoverer extends Discoverer {
     public List<Device> discoverDevices() throws IOException, InterruptedException, ExecutionException {
+        ChromeCasts.startDiscovery();
+        ChromeCasts.get();
+
 
     }
 }
