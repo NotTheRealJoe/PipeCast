d93d1c43c030ecb396ed15fb3ee10ad06922165e
NotTheRealJoe <32490063+NotTheRealJoe@users.noreply.github.com>
2018-09-28 10:49:26 -0500

    Finished hooking up to library for discoverer

--- src/main/java/org/schabi/newpipe/cast/protocols/chromecast/ChromecastDiscoverer.java
+++ src/main/java/org/schabi/newpipe/cast/protocols/chromecast/ChromecastDiscoverer.java
@@ -5,16 +5,30 @@ import org.schabi.newpipe.cast.Discoverer;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
 import su.litvak.chromecast.api.v2.ChromeCasts;
 import su.litvak.chromecast.api.v2.ChromeCast;
 
 public class ChromecastDiscoverer extends Discoverer {
-    public List<Device> discoverDevices() throws IOException, InterruptedException, ExecutionException {
+
+    public ChromecastDiscoverer() throws IOException {
         ChromeCasts.startDiscovery();
-        ChromeCasts.get();
+    }
 
+    public List<Device> discoverDevices() throws IOException, InterruptedException, ExecutionException {
+        List<Device> foundDevices = new ArrayList<>();
 
+        for(ChromeCast chromecast : ChromeCasts.get()) {
+            foundDevices.add(new ChromecastDevice(chromecast));
+        }
+
+        return foundDevices;
+    }
+
+    @Override
+    public void finalize() throws IOException {
+        ChromeCasts.stopDiscovery();
     }
 }
